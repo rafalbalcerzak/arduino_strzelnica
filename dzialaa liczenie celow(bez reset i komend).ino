e5bacb6e6d720f678496b85132f0482b93416500
rafalbalcerzak <raffi.balcer@gmail.com>
2017-09-30 12:29:27 +0200

    Create dzialaa liczenie celow(bez reset i komend).ino

---
+++ dzialaa liczenie celow(bez reset i komend).ino
@@ -0,0 +1,48 @@
+int led []={7, 8, 9, 10};
+int button []={2, 3, 4, 5};
+int buttonState[] = {0,0,0,0};
+char input;
+int x = random(0,4);//numer popera
+int xOld = x;
+int current =0;
+int target =20;//cos nie tak
+int flag = 0;
+
+void setup() {
+  pinMode(led[0], OUTPUT);
+  pinMode(led[1], OUTPUT);
+  pinMode(led[2], OUTPUT);
+  pinMode(led[3], OUTPUT);
+  pinMode(button[0], INPUT);
+  pinMode(button[1], INPUT);
+  pinMode(button[2], INPUT);
+  pinMode(button[3], INPUT);
+  Serial.begin(9600);
+  Serial.println("ready");
+  randomSeed(analogRead(0));
+}
+
+void loop() {
+  if(current < target){
+    digitalWrite(led[x],HIGH);
+    buttonState[x]=digitalRead(button[x]);
+    
+    if(buttonState[x] == HIGH){
+      digitalWrite(led[x],LOW);
+      do{
+      x = random(0,4);
+      }while(x == xOld);
+      
+      if(x!=xOld){
+        xOld=x;
+        current ++;
+      }
+      Serial.println(current);
+    }
+  }
+  
+  else if(flag ==0){
+    flag = 1;
+    Serial.println("koniec");
+  }
+}
